8836de8bd5ff98445e363174beb0ae5ef9bc1cf0
Padunk <abraham.agel@gmail.com>
2018-11-22 10:53:29 +0700

    finish steamroller challenge

--- intermediate-algorithm-scripting/steamroller.js
+++ intermediate-algorithm-scripting/steamroller.js
@@ -2,6 +2,16 @@
 Flatten a nested array. You must account for varying levels of nesting.
 */
 
+// recursive function
 function steamrollArray(arr) {
-   
+   if (Array.isArray(arr)) {
+		return arr.reduce((a, e) => a.concat(steamrollArray(e)), []);
+	}
+	return arr;
+}
+
+// using flat, but hardcode the flat depth
+// TODO, don't hardcode it.
+function steamrollArray(arr) {
+   return arr.flatMap(e => Array.isArray(e) ? e.flat(2) : e);
 }
