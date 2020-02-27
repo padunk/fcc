879c615b42f0b1e5f5214b360949445533b011dc
Padunk <abraham.agel@gmail.com>
2020-02-27 08:37:05 +0700

    initial commit

---
+++ data-visualization/choropleth/src/choropleth.js
@@ -0,0 +1,8 @@
+"use strict";
+const heatMap = document.querySelector("#usa-map");
+const legend = document.querySelector("#legend");
+
+const educationUrl =
+    "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json";
+const countyUrl =
+    "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json";
