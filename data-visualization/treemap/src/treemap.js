17b059f2bf507d490b4ccf609e9a895afc575386
Padunk <abraham.agel@gmail.com>
2020-02-29 15:05:30 +0700

    treemap initial commit

---
+++ data-visualization/treemap/src/treemap.js
@@ -0,0 +1,9 @@
+"use strict";
+const mapWrapper = document.querySelector("#tree-map");
+const legend = document.querySelector("#legend");
+
+function init() {
+    document.addEventListener("DOMContentLoaded", renderChoropleth);
+}
+
+init();
